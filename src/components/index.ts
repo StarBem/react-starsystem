d7258e22bf3e9ab58e1c8fbd1df3b89c8958b09b
Julio Augusto <julioams@icloud.com>
2022-03-04 13:38:06 -0300

    feat: export configs by theme

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,27 @@
 import Button, { ButtonProps } from "./Button";
+
+import {
+  Colors,
+  colors,
+  ThemeProvider,
+  ThemeConsumer,
+  ThemeContext,
+  withTheme,
+  UpdateTheme,
+  ReplaceTheme,
+  FullTheme,
+  Theme,
+  ThemeProps,
+} from "./config";
+
+// Export Components
 export { Button };
-export type { ButtonProps };
+
+// Export Theme utils
+export { colors, ThemeProvider, ThemeConsumer, ThemeContext, withTheme };
+
+// Components Props Exports
+export type { ButtonProps, Colors };
+
+// Theme Props Exports
+export type { FullTheme, ReplaceTheme, Theme, ThemeProps, UpdateTheme };
